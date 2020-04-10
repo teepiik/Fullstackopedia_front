73030886effcada075b5d5ad90565c1b283bd94a
teepiik <teemu.piik@gmail.com>
2020-04-10 10:10:24 +0300

    custom hook removed, unnecessary

--- src/Hooks/hooks.js
+++ src/Hooks/hooks.js
@@ -16,32 +16,3 @@ export const useField = type => {
         field: { type, value, onChange }
     }
 }
-
-// TODO Unfinished hook for Select fields
-
-export const useSelectField = choices => {
-    const [value, setValue] = useState('') // is needed?
-    const type = 'select'
-
-    const onChange = (event) => {
-        setValue(event.target.value)
-    }
-
-    const setEmpty = () => {
-        setValue('')
-    }
-
-    // check choices --- need to add something for ex. ['a', 'b']
-    const makeRows = () => {
-        choices.map(c =>
-            <option key={c} value={c}>{c}</option>
-        )
-    }
-
-    return {
-        makeRows: makeRows,
-        setEmpty: setEmpty,
-        value: value, // current selected value
-        field: { type, onChange }
-    }
-}
